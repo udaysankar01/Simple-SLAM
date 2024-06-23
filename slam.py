8c174fda0b9a619b54b80e6226a4a9a4014488c0
Uday Sankar <udaysankar.ambadi@gmail.com>
2024-06-22 22:08:56 -0400

    pangolin visualization with thread

--- slam.py
+++ slam.py
@@ -6,6 +6,10 @@ from frame import Frame
 import g2o
 from utils import *
 
+from multiprocessing import Process, Queue
+import pangolin
+import OpenGL.GL as gl
+
 W = 1920//2
 H = 1080//2
 F = 280
@@ -19,17 +23,65 @@ class Map(object):
         self.frames = []
         self.points = []
 
+        # create viewer process
+        self.q = Queue()
+        self.viewer_process = Process(target=self.viewer_thread, args=(self.q,))
+        self.viewer_process.daemon = True
+        self.state = None
+        self.viewer_process.start()
+
+    def viewer_thread(self, q):
+        self.viewerInit()
+
+        self.state = None
+        flag = True
+        while not pangolin.ShouldQuit():
+            self.viewerUpdate(q)
+
+    
+    def viewerInit(self):
+        pangolin.CreateWindowAndBind('SLAM', 640, 480)
+        gl.glEnable(gl.GL_DEPTH_TEST)
+
+        # Define Projection and initial ModelView matrix
+        self.scam = pangolin.OpenGlRenderState(
+            pangolin.ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
+            pangolin.ModelViewLookAt(-2, 2, -2, 0, 0, 0, pangolin.AxisDirection.AxisY))
+        self.handler = pangolin.Handler3D(self.scam)
+
+        # Create Interactive View in window
+        self.dcam = pangolin.CreateDisplay()
+        self.dcam.SetBounds(0.0, 1.0, 0.0, 1.0, -640.0/480.0)
+        self.dcam.SetHandler(self.handler)
+
+    def viewerUpdate(self, q):
+        if self.state is None or not q.empty():
+                self.state = q.get()
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+        gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+        self.dcam.Activate(self.scam)
+
+        gl.glPointSize(10)
+        gl.glColor(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(np.array([d[:3, 3] for d in self.state[0]]))
+                
+        gl.glPointSize(2)
+        gl.glColor(0.0, 1.0, 0.0)
+        pangolin.DrawPoints(np.array(self.state[1]))   
+
+        pangolin.FinishFrame()
+
+    def stop_viewer(self):
+        self.viewer_process.terminate()
+        self.viewer_process.join()
+
     def display(self):
-        for frame in self.frames:
-            print(frame.id)
-            print(frame.Rt)
-            print('\n')
-        
-        # for point in self.points:
-        #     print(point.id)
+        Rts = [frame.Rt for frame in self.frames]
+        pts = [point.position for point in self.points]
+        self.q.put((Rts, pts))
 
 
-slam_map = Map()
+slam_map = Map()    
 
 class Point(object):
     
@@ -45,6 +97,7 @@ class Point(object):
     def add_observation(self, frame, idx):
         self.frames.append(frame)
         self.idxs.append(idx)
+
 
 
 def triangulate(pose1, pose2, pts1, pts2):
@@ -87,10 +140,12 @@ def main():
                 break
         else:
             break
+    
 
     cap.release()
     cv2.destroyAllWindows()
-
+    slam_map.stop_viewer()
+    
 
 if __name__ == '__main__':
     main()
